61c513df0c572ea07f7714ccfae24fe686a2e78a
Documenter.jl <documenter@juliadocs.github.io>
2023-07-17 07:41:24 +0000

    build based on 1a0afa4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraphResolvingSets","category":"page"},{"location":"#GraphResolvingSets","page":"Home","title":"GraphResolvingSets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraphResolvingSets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraphResolvingSets]","category":"page"}]
+}
